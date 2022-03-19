bc59b2021bf3446147cc985d41b37a2e8067d446
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-03-19 10:27:09 +0530

    initial commit

---
+++ eventData.js
@@ -0,0 +1,47 @@
+
+ const data=[
+	{
+		"text": "textOne",
+		"dateTime": "2022-03-19 10:18:35.000"
+   },
+{
+		"text": "textTwo",
+		"dateTime": "2022-03-19 10:18:39.000"
+},
+
+{
+    "text": "textThree",
+    "dateTime": "2022-03-19 10:18:00.000"
+},
+{
+    "text": "textFour",
+    "dateTime": "2022-03-19 10:18:35.000"
+},
+{
+    "text": "textFive",
+    "dateTime": "2022-03-19 10:03:35.000"
+},
+{
+    "text": "textSix",
+    "dateTime": "2022-03-19 10:03:35.000"
+},
+{
+    "text": "textSeven",
+    "dateTime": "2022-03-19 10:03:35.000"
+},
+{
+    "text": "textEight",
+    "dateTime": "2022-03-19 10:03:35.000"
+},
+{
+    "text": "textNine",
+    "dateTime": "2022-03-19 10:03:35.000"
+},
+
+{
+		"text": "textFinal",
+		"dateTime": "2022-03-19 10:03:35.000"
+}
+]
+
+module.exports=data
---
+++ schedular.js
@@ -0,0 +1,28 @@
+const data = require("./eventData");
+const date = require("date-and-time");
+const schedule = require("node-schedule");
+const now = new Date();
+
+// Formatting the date and time
+// by using date.format() method
+const value = date.format(now, "YYYY/MM/DD HH:mm:ss:SSS");
+
+//loop through all the event
+data.map((item) => {
+  console.log(item.text, item.dateTime);
+  console.log(value);
+  var task = item.text;
+  var len = item.text.length;
+  var time = item.dateTime;
+
+  //schedule the job at particular time
+
+  schedule.scheduleJob(task, time, () => {
+    console.log(`event ${text}triggered`);
+
+    // waiting for the time while is equivalent to the length of string in seconds
+    setTimeout(function callbk() {
+      console.log(`your task run ${text}`);
+    }, len * 1000);
+  });
+});
