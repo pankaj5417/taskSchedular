4c6251e98ee89ae784949df2b29b54172aadf342
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-03-19 10:47:39 +0530

    few updates

--- schedular.js
+++ schedular.js
@@ -3,11 +3,14 @@ const date = require("date-and-time");
 const schedule = require("node-schedule");
 const now = new Date();
 
+//var task=document.getElementById("event")
+
 // Formatting the date and time
 // by using date.format() method
 const value = date.format(now, "YYYY/MM/DD HH:mm:ss:SSS");
 
 //loop through all the event
+
 data.map((item) => {
   console.log(item.text, item.dateTime);
   console.log(value);
